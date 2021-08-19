c0c86528130e25a4a892526507ca11e2fb6fa684
gonzaloperrone <gonzalodamianperrone@gmail.com>
2021-08-19 16:38:05 -0300

    act

--- js/products.js
+++ js/products.js
@@ -1,38 +1,13 @@
-const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
 var currentProductsArray = [];
-var currentSortCriteria = undefined;
 
-
-function sortProducts(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
-            return 0;
-        });
-    }
-
-    return result;
-}
-
-function showProductsList(){
+function showCategoriesList(array) {
 
     let htmlContentToAppend = "";
-    
-    for(let i = 0; i < currentProductsArray.length; i++){
-        let product = currentProductsArray[i];
-       
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
 
-            htmlContentToAppend += `
+
+        htmlContentToAppend += `
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
@@ -40,42 +15,30 @@ function showProductsList(){
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1" style="font-size: 200%;">`+ product.name +`</h4>
+                            <h4 class="mb-1" style="font-size: 200%;">`+ product.name + `</h4>
                             <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
                         </div><br>
-                        <b class="mb-1" style="font-size: 130%;">` + product.currency +  ` ` + product.cost +  ` </b><br>
+                        <b class="mb-1" style="font-size: 130%;">` + product.currency + ` ` + product.cost + ` </b><br>
                         <br>
                         <p class="mb-1">` + product.description + `</p>
                     </div>
                 </div>
             </a>
             `
-        
+
 
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
     }
 }
 
-function sortAndShowProducts(sortCriteria, productsArray){
-    currentSortCriteria = sortCriteria;
-
-    if(productsArray != undefined){
-        currentProductsArray = productsArray;
-    }
-
-    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
-
-    showProductsList();
-}
 
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            sortAndShowProducts(ORDER_ASC_BY_NAME, resultObj.data);
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray);
         }
     });
-
-    
-
-    });
+});

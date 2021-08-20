bc908e5fcb866113a3c9e87009d497df0e1d0bd7
gonzaloperrone <gonzalodamianperrone@gmail.com>
2021-08-20 11:19:57 -0300

    act

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
 function miFuncion() {
+
+  let user = document.getElementById("user").value;
+  let pass = document.getElementById("pass").value;
+  if((user !=="") &&(pass !=="")){
     window.location = "inicio.html";
   }
+  else{
+    alert("Debe completar los campos")
+  }
+}

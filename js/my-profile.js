124215caf11512e14c653ee5d51bd7adc730034f
gonzaloperrone <gonzalodamianperrone@gmail.com>
2021-10-22 08:52:29 -0300

    aa

--- js/my-profile.js
+++ js/my-profile.js
@@ -20,8 +20,10 @@ $(document).ready(function () {
 function saveData(){
     let data = {
         //img: document.getElementById("file-upload").value,
-        name: document.getElementById("name").value,
-        lastName: document.getElementById("lastName").value,
+        name1: document.getElementById("name1").value,
+        name2: document.getElementById("name2").value,
+        lastName1: document.getElementById("lastName1").value,
+        lastName2: document.getElementById("lastName2").value,
         age: document.getElementById("age").value,
         email: document.getElementById("email").value,
         phone: document.getElementById("phone").value
@@ -36,8 +38,10 @@ document.getElementById("saveButton").addEventListener("click",function(e){
 document.addEventListener("DOMContentLoaded", function(e){
     let allData = JSON.parse(localStorage.getItem("profile"));
     //document.getElementById("file-upload").value= allData.file-upload;
-    document.getElementById("name").value= allData.name;
-    document.getElementById("lastName").value= allData.lastName;
+    document.getElementById("name1").value= allData.name1;
+    document.getElementById("name2").value= allData.name2;
+    document.getElementById("lastName1").value= allData.lastName1;
+    document.getElementById("lastName2").value= allData.lastName2;
     document.getElementById("age").value= allData.age;
     document.getElementById("email").value= allData.email;
     document.getElementById("phone").value= allData.phone;
